3c59bf42ce91afe139c38b352249d3d5fd5e3365
Compotes <teamcompotes@gmail.com>
2018-01-20 13:04:19 +0100

    Thresholding

--- Makefile
+++ Makefile
@@ -18,7 +18,7 @@ LFLAGS =
 all: $(PROGRAM)
 
 $(PROGRAM): xiApiPlusOcv.o OmniOperations.o ProjectVersion.o ximeaJetsonGPU.o Unwrapper.o
-	g++ $(OBJECTS) -o $@ -lm3api -lopencv_core -lopencv_highgui -lopencv_imgproc -lopencv_imgcodecs -lopencv_cudaimgproc -lopencv_cudafilters -lopencv_xphoto -lcudart -lopencv_features2d -lopencv_cudaarithm -L/root/opencv-3.3.0/build/lib -L$(CUDAPATH)/lib64
+	g++ $(OBJECTS) -o $@ -lm3api -lopencv_core -lopencv_highgui -lopencv_imgproc -lopencv_imgcodecs -lopencv_cudaimgproc -lopencv_cudafilters -lcudart -lopencv_features2d -lopencv_cudaarithm -L/root/opencv-3.3.0/build/lib -L$(CUDAPATH)/lib64
 
 xiApiPlusOcv.o: xiApiPlusOcv.cpp
 	g++ -c $< -I . -I /root/opencv-3.3.0/build/include -I /root/opencv_contrib-3.3.0/modules/xphoto/include -I /usr/local/cuda/include -g3 -Wall -c -fmessage-length=0
@@ -33,11 +33,15 @@ ProjectVersion.o: ProjectVersion.cpp
 	g++ -c $< -I .  -I /root/opencv-3.3.0/build/include -g3 -Wall -c -fmessage-length=0
 
 Unwrapper.o: Unwrapper.cu
-	$(NVCC) -ccbin g++ -m64 -gencode arch=compute_53,code=sm_53 -gencode arch=compute_60,code=sm_60 -gencode arch=compute_62,code=sm_62 -gencode arch=compute_62,code=compute_62 $(NVCCFLAGS) $(LFLAGS) -o $@ -c $<
+	$(NVCC) -ccbin g++ -m64 -gencode arch=compute_62,code=sm_62 -gencode arch=compute_62,code=sm_62 -gencode arch=compute_62,code=sm_62 -gencode arch=compute_62,code=compute_62 $(NVCCFLAGS) $(LFLAGS) -o $@ -c $<
 
 runX11: $(PROGRAM)
 	cat ~nvidia/.Xauthority > ~/.Xauthority
 	LD_LIBRARY_PATH=/root/opencv-3.3.0/build/lib DISPLAY=localhost:10.0 ./ximeaJetsonGPU
+
+run: $(PROGRAM)
+	cat ~nvidia/.Xauthority > ~/.Xauthority
+	LD_LIBRARY_PATH=/root/opencv-3.3.0/build/lib ./ximeaJetsonGPU
 
 runGui: $(PROGRAM)
 	rm -f /tmp/*.png
--- ximeaJetsonGPU.cpp
+++ ximeaJetsonGPU.cpp
@@ -4,47 +4,67 @@
 #include "xiApiPlusOcv.hpp"
 #include <time.h>
 
+#include <errno.h>
+#include <fcntl.h> 
+#include <string.h>
+#include <termios.h>
+#include <unistd.h>
+
+#define EXPOSURE_TIME 10000
+#define SHOW_IMG true
+#define FILTER 256
+
 using namespace std;
 using namespace cv;
 using namespace cv::cuda;
 
 struct timespec t0, t1;
+struct termios tty;
+
 unsigned int counter = 0;
+unsigned int fps = 0;
 
 GpuMat outGpu, outGpu2, hsvGpu;
+GpuMat shsv[3];
+GpuMat thresc[3];
+GpuMat inGpu;
+Mat mat;
 Mat dst;
+Mat thrMat;
+Mat hsvMat;
+xiAPIplusCameraOcv cam;
 
 byte *matToBytes(Mat image)
 {
-   int size = image.total() * image.elemSize();
-   byte *bytes = new byte[size];  // you will have to delete[] that later
-   std::memcpy(bytes,image.data,size * sizeof(byte));
-   return bytes;
+    int size = image.total() * image.elemSize();
+    byte *bytes = new byte[size];  // you will have to delete[] that later
+    std::memcpy(bytes,image.data,size * sizeof(byte));
+    return bytes;
 }
 
 Mat *uMat;
 //cv::SimpleBlobDetector detector;
 
 void unwrapAndSave(Mat *mat, float a, float b, double s) {
-  //printf("Unwrapping %.1f %.1f %.1f\n", a, b, s);
-  OmniOperations *omni = new OmniOperations(
-      mat->size().width / 2.0f,
-      mat->size().height / 2.0f,
-      mat->size().height / 2.0f,
-      57.0f, 
-      a, 
-      b,
-      s
-      );
+    //printf("Unwrapping %.1f %.1f %.1f\n", a, b, s);
+    OmniOperations *omni = new OmniOperations(
+            mat->size().width / 2.0f,
+            mat->size().height / 2.0f,
+            mat->size().height / 2.0f,
+            57.0f, 
+            a, 
+            b,
+            s
+            );
 
-  // char fname[128];
-  // sprintf(fname, "/tmp/test_omni_%.1f_%.1f_%.1f.png", a, b, s);
-  uMat = omni->unwrap(*mat, false);
-  //result->copyTo(uMat);
+    // char fname[128];
+    // sprintf(fname, "/tmp/test_omni_%.1f_%.1f_%.1f.png", a, b, s);
+    uMat = omni->unwrap(*mat, false);
+    //result->copyTo(uMat);
 
-  //imwrite(fname, *result);
+    //imwrite(fname, *result);
 
-  delete omni;
+    delete omni;
 }
 
 void measureFps() {
@@ -52,45 +72,151 @@ void measureFps() {
     uint64_t deltaTime = (t1.tv_sec - t0.tv_sec) * 1000000 + (t1.tv_nsec - t0.tv_nsec) / 1000 / 1000;
     counter++;
     if (deltaTime > 1000) {
-      printf("FPS %10d\n", counter);
-      counter = 0;
-      t0 = t1;
+        fps = counter;
+        printf("FPS %10d\n", fps);
+        counter = 0;
+        t0 = t1;
     }
 }
 
+int init_serial(int fd, int speed, int parity)
+{
+    memset(&tty, 0, sizeof(tty));
+    if (tcgetattr (fd, &tty) != 0)
+    {
+        printf ("error %d from tcgetattr", errno);
+        return -1;
+    }
+
+    cfsetospeed (&tty, speed);
+    cfsetispeed (&tty, speed);
+
+    tty.c_cflag = (tty.c_cflag & ~CSIZE) | CS8;     // 8-bit chars
+    // disable IGNBRK for mismatched speed tests; otherwise receive break
+    // as \000 chars
+    tty.c_iflag &= ~IGNBRK;         // disable break processing
+    tty.c_lflag = 0;                // no signaling chars, no echo,
+    // no canonical processing
+    tty.c_oflag = 0;                // no remapping, no delays
+    tty.c_cc[VMIN]  = 0;            // read doesn't block
+    tty.c_cc[VTIME] = 5;            // 0.5 seconds read timeout
+
+    tty.c_iflag &= ~(IXON | IXOFF | IXANY); // shut off xon/xoff ctrl
+
+    tty.c_cflag |= (CLOCAL | CREAD);// ignore modem controls,
+    // enable reading
+    tty.c_cflag &= ~(PARENB | PARODD);      // shut off parity
+    tty.c_cflag |= parity;
+    tty.c_cflag &= ~CSTOPB;
+    tty.c_cflag &= ~CRTSCTS;
+
+    if (tcsetattr (fd, TCSANOW, &tty) != 0)
+    {
+        printf ("error %d from tcsetattr", errno);
+        return -1;
+    }
+    return 0;
+}
+
+    void
+set_blocking (int fd, int should_block)
+{
+    struct termios tty;
+    memset (&tty, 0, sizeof tty);
+    if (tcgetattr (fd, &tty) != 0)
+    {
+        printf ("error %d from tggetattr", errno);
+        return;
+    }
+
+    tty.c_cc[VMIN]  = should_block ? 1 : 0;
+    tty.c_cc[VTIME] = 5;            // 0.5 seconds read timeout
+
+    if (tcsetattr (fd, TCSANOW, &tty) != 0)
+        printf ("error %d setting term attributes", errno);
+}
+
+void stop() {
+    cam.StopAcquisition();
+    cam.Close();
+    printf("Done\n");	
+    exit(0);
+}
+
+unsigned int debugCounter = 0;
+void debugImage(Mat *resultMat) {
+    if (SHOW_IMG && debugCounter > 50) {
+        char fpsStr[32];
+        sprintf(fpsStr, "%10d", fps);
+        rectangle(mat, Point2f(0, 0), Point2f(2000, 40), Scalar(255, 255, 255, 255), CV_FILLED);
+        putText(mat, fpsStr, Point2f(0, 30), FONT_HERSHEY_PLAIN, 2, Scalar(0,0,0,255));
+
+        imshow("input", mat);
+
+        if (resultMat != NULL)
+            imshow("result", *resultMat);
+
+        debugCounter = 0;
+    } else
+        debugCounter++;
+
+    if (cv::waitKey(1) == 27) {stop();}
+    measureFps();
+}
 int main(int argc, char* argv[])
 {
-    namedWindow("input", WINDOW_AUTOSIZE);
-    namedWindow("result", WINDOW_AUTOSIZE);
-    namedWindow("gui", WINDOW_AUTOSIZE);
+    namedWindow("input", WINDOW_NORMAL);
+    namedWindow("result", WINDOW_NORMAL);
+    namedWindow("gui", WINDOW_NORMAL);
+
+    resizeWindow("input", 1000, 400);
+    resizeWindow("result", 1000, 400);
+    resizeWindow("gui", 200, 800);
 
     moveWindow("input", 0, 0);
-    moveWindow("result", 0, 1440/2);
-    moveWindow("gui", 1300, 0);
+    moveWindow("result", 0, 475);
+    moveWindow("gui", 1450, 0);
 
-	xiAPIplusCameraOcv cam;
+    // char *portname = "/dev/ttyTHS2";
+    // int fd = open (portname, O_RDWR | O_NOCTTY | O_SYNC);
+    // if (fd < 0)
+    // {
+    //     printf ("error %d opening %s: %s", errno, portname, strerror (errno));
+    //     return 0;
+    // }
 
-	// Retrieving a handle to the camera device
-	printf("Opening first camera...\n");
-	cam.OpenFirst();
-	
-	// Set exposure
-	//cam.SetExposureTime(10000); //10000 us = 10 ms
-	cam.SetExposureTime(10000); //10000 us = 10 ms
-	cam.SetDownsamplingType(XI_SKIPPING);
-	cam.SetDownsampling(cam.GetDownsampling_Maximum());
-	//cam.SetGain(20.0f);
-	cam.SetImageDataFormat(XI_RAW8);
-	
-	printf("Starting acquisition...\n");
-	cam.StartAcquisition();
-	
-	printf("First pixel value \n");
-	XI_IMG_FORMAT format = cam.GetImageDataFormat();
-	printf("XI_IMG_FORMAT=%d\n", format);
+    // init_serial (fd, B115200, 0);  // set speed to 115,200 bps, 8n1 (no parity)
+    // set_blocking (fd, 0);                // set no blocking
+
+    // while (1) {
+    //     write (fd, "hello!\n", 7);           // send 7 character greeting
+
+    //     usleep (1 * 1000 * 1000);
+    // }
+
+
+
+    // Retrieving a handle to the camera device
+    printf("Opening first camera...\n");
+    cam.OpenFirst();
+
+    // Set exposure
+    //cam.SetExposureTime(10000); //10000 us = 10 ms
+    cam.SetExposureTime(EXPOSURE_TIME); //10000 us = 10 ms
+    cam.SetDownsamplingType(XI_SKIPPING);
+    cam.SetDownsampling(cam.GetDownsampling_Maximum());
+    //cam.SetGain(20.0f);
+    cam.SetImageDataFormat(XI_RAW8);
+
+    printf("Starting acquisition...\n");
+    cam.StartAcquisition();
+
+    printf("First pixel value \n");
+    XI_IMG_FORMAT format = cam.GetImageDataFormat();
+    printf("XI_IMG_FORMAT=%d\n", format);
 
     printf("Get XI_PRM_COLOR_FILTER_ARRAY\n");
-		XI_COLOR_FILTER_ARRAY colorFilterArray = cam.GetSensorColorFilterArray();
+    XI_COLOR_FILTER_ARRAY colorFilterArray = cam.GetSensorColorFilterArray();
     printf("XI_PRM_COLOR_FILTER_ARRAY=%d\n", colorFilterArray);
 
     int val1 =  21; createTrackbar("Hmin",        "gui", &val1,  255, 0);
@@ -101,157 +227,151 @@ int main(int argc, char* argv[])
     int val6 = 255; createTrackbar("Vmax",        "gui", &val6,  255, 0);
     int val7 = 300; createTrackbar("elementSize", "gui", &val7, 1000, 0);
     int val8 =   0; createTrackbar("val8",        "gui", &val8,  255, 0);
-		
-    cv::Ptr<cv::xphoto::WhiteBalancer> wb = cv::xphoto::createSimpleWB();
+
+    //cv::Ptr<cv::xphoto::WhiteBalancer> wb = cv::xphoto::createSimpleWB();
     counter = 0;
     clock_gettime(CLOCK_REALTIME, &t0);
 
     GpuMat temp;
     GpuMat thres;
     GpuMat toInv;
-		
+
     for (;;)
-		{
-			Mat mat = cam.GetNextImageOcvMat();
-      
-//      imshow("result", mat);
-//      measureFps();
-  //    cv::waitKey(1);
+    {
+        mat = cam.GetNextImageOcvMat();
 
-      const GpuMat inGpu(mat);
-      cv::cuda::demosaicing(inGpu, outGpu, COLOR_BayerBG2BGR);
-      Mat demosMat;
-      outGpu.download(demosMat);
-      
-      // Mat testMat;
-/*       imshow("result", demosMat);
-       cv::waitKey(1);
-       measureFps();
-       continue;
-*/
+        //  imshow("result", mat);
+        //  measureFps();
+        //  cv::waitKey(1);
 
-      //Mat wbMat;
-      //wb->balanceWhite(demosMat, wbMat);
+        inGpu.upload(mat);
+        cv::cuda::demosaicing(inGpu, outGpu, FILTER);
+        //Mat demosMat;
+        //outGpu.download(demosMat);
 
-      //imshow("result", wbMat);
-      //cv::waitKey(1);
-      //continue;
+        // Mat testMat;
 
-      unwrapAndSave(&demosMat, 1.0f, 0.5f, 0.1);
-      //imshow("unwrapkokotpica", demosMat);
-      //cv::waitKey(1);
+        // continue;
 
-      // HSV threshold
-      const GpuMat inUmat(*uMat);
-      cv::cuda::cvtColor(inUmat, hsvGpu, COLOR_BGR2HSV);
+        //Mat wbMat;
+        //wb->balanceWhite(demosMat, wbMat);
 
-      //Split HSV 3 channels
-      GpuMat shsv[3];
-      cv::cuda::split(hsvGpu, shsv);
+        //imshow("result", wbMat);
+        //cv::waitKey(1);
+        //continue;
 
-      //Threshold HSV channels
-      GpuMat thresc[3];
-      cv::cuda::threshold(shsv[0], thresc[0], val1, val4, THRESH_BINARY);
-      cv::cuda::threshold(shsv[1], thresc[1], val2, val5, THRESH_BINARY);
-      cv::cuda::threshold(shsv[2], thresc[2], val3, val6, THRESH_BINARY);
+        //unwrapAndSave(&demosMat, 1.0f, 0.5f, 0.1);
+        //imshow("unwrapkokotpica", demosMat);
+        //cv::waitKey(1);
 
-      //Bitwise AND the channels
-      cv::cuda::bitwise_and(thresc[0], thresc[1], temp);
-      cv::cuda::bitwise_and(temp, thresc[2], toInv);
-      cv::cuda::bitwise_not(toInv, thres);
+        // HSV threshold
+        //const GpuMat inUmat(*uMat);
+        cv::cuda::cvtColor(outGpu, hsvGpu, COLOR_BGR2HSV);
 
-      // Mat thrMat;
-      // thres.download(thrMat);
-      // Mat hsvMat;
-      // hsvGpu.download(hsvMat);
-      // Mat thrMat;
-      // inRange(hsvMat, cv::Scalar(val1, val2, val3), cv::Scalar(val4, val5, val6), thrMat);
+        //Split HSV 3 channels
+        cv::cuda::split(hsvGpu, shsv);
+
+        //Threshold HSV channels
+        cv::cuda::threshold(shsv[0], thresc[0], val1, val4, THRESH_BINARY);
+        cv::cuda::threshold(shsv[1], thresc[1], val2, val5, THRESH_BINARY);
+        cv::cuda::threshold(shsv[2], thresc[2], val3, val6, THRESH_BINARY);
+
+        //Bitwise AND the channels
+        cv::cuda::bitwise_and(thresc[0], thresc[1], temp);
+        cv::cuda::bitwise_and(temp, thresc[2], toInv);
+        cv::cuda::bitwise_not(toInv, thres);
+
+        thres.download(thrMat);
+        hsvGpu.download(hsvMat);
 
 
+        inRange(hsvMat, cv::Scalar(val1, val2, val3), cv::Scalar(val4, val5, val6), thrMat);
 
-      //std::vector<KeyPoint> keypoints;
-      //detector.detect(thrMat, keypoints);
-
-      // Mat blobMat;
-      // drawKeypoints(uMat, keypoints, blobMat, Scalar(0,0,255), DrawMatchesFlags::DRAW_RICH_KEYPOINTS);
-
-      //const GpuMat filterMat(thrMat);
-
-      GpuMat filterMat = thres;
-      GpuMat erodeMat, dilateMat;
-
-      int elementShape = MORPH_RECT;
-      float filterArea = val7 / 100.0f;
-      Mat element = getStructuringElement(elementShape, Size(filterArea, filterArea), Point(filterArea / 2.0f, filterArea / 2.0f));
-      Ptr<cuda::Filter> erodeFilter = cuda::createMorphologyFilter(MORPH_ERODE, filterMat.type(), element);
-      erodeFilter->apply(filterMat, erodeMat);
-      Ptr<cuda::Filter> dilateFilter = cuda::createMorphologyFilter(MORPH_DILATE, erodeMat.type(), element);
-      dilateFilter->apply(erodeMat, dilateMat);
-
-      Mat filteredMat;
-      dilateMat.download(filteredMat);
+        debugImage(&thrMat);
+        continue;
 
 
-      cv::SimpleBlobDetector::Params params;
-      params.filterByColor = 1;
-      params.blobColor = 255;
-      params.minThreshold = 10;
-      params.maxThreshold = 220;
-      params.filterByArea = true;
-      params.minArea = 0;
-      params.filterByCircularity = true;
-      params.minCircularity = 0.5f;
-      params.maxCircularity = 1.0f;
-      params.filterByConvexity = false;
-      params.filterByInertia = false;
-      cv::Ptr<cv::SimpleBlobDetector> detector = cv::SimpleBlobDetector::create(params);
-      vector<KeyPoint> blobList;
-      detector->detect(filteredMat, blobList);;
+        //std::vector<KeyPoint> keypoints;
+        //detector.detect(thrMat, keypoints);
 
-      //Mat testMat;
-      //dilateMat.download(testMat);
-      //imshow("result", testMat);
-      //cv::waitKey(1);
-      //measureFps();
-      //continue;
+        // Mat blobMat;
+        // drawKeypoints(uMat, keypoints, blobMat, Scalar(0,0,255), DrawMatchesFlags::DRAW_RICH_KEYPOINTS);
 
-      Mat blankMat = Mat::zeros(filteredMat.size(), filteredMat.type());
-      Mat blobMat;
-      drawKeypoints(blankMat, blobList, blobMat, Scalar(255, 255, 255), DrawMatchesFlags::DRAW_RICH_KEYPOINTS);
+        //const GpuMat filterMat(thrMat);
+
+        /*GpuMat filterMat = thres;
+          GpuMat erodeMat, dilateMat;
+
+          int elementShape = MORPH_RECT;
+          float filterArea = val7 / 100.0f;
+          Mat element = getStructuringElement(elementShape, Size(filterArea, filterArea), Point(filterArea / 2.0f, filterArea / 2.0f));
+          Ptr<cuda::Filter> erodeFilter = cuda::createMorphologyFilter(MORPH_ERODE, filterMat.type(), element);
+          erodeFilter->apply(filterMat, erodeMat);
+          Ptr<cuda::Filter> dilateFilter = cuda::createMorphologyFilter(MORPH_DILATE, erodeMat.type(), element);
+          dilateFilter->apply(erodeMat, dilateMat);
+
+          Mat filteredMat;
+          dilateMat.download(filteredMat);
 
 
-      Mat resultMat = blobMat;
-      imshow("input", *uMat);
-      imshow("result", resultMat);
-      cv::waitKey(1);
+          cv::SimpleBlobDetector::Params params;
+          params.filterByColor = 1;
+          params.blobColor = 255;
+          params.minThreshold = 10;
+          params.maxThreshold = 220;
+          params.filterByArea = true;
+          params.minArea = 0;
+          params.filterByCircularity = true;
+          params.minCircularity = 0.5f;
+          params.maxCircularity = 1.0f;
+          params.filterByConvexity = false;
+          params.filterByInertia = false;
+          cv::Ptr<cv::SimpleBlobDetector> detector = cv::SimpleBlobDetector::create(params);
+          vector<KeyPoint> blobList;
+          detector->detect(filteredMat, blobList);;*/
 
-      //break;
+        //Mat testMat;
+        //dilateMat.download(testMat);
+        //imshow("result", testMat);
+        //cv::waitKey(1);
+        //measureFps();
+        //continue;
+
+        /*Mat blankMat = Mat::zeros(filteredMat.size(), filteredMat.type());
+          Mat blobMat;
+          drawKeypoints(blankMat, blobList, blobMat, Scalar(255, 255, 255), DrawMatchesFlags::DRAW_RICH_KEYPOINTS);
 
 
-       //cv::imshow("jetson", *uMat);
-       //cv::waitKey(1);
+          Mat resultMat = blobMat;
+          imshow("input", *uMat);
+          imshow("result", resultMat);
+          cv::waitKey(1); */
+
+        //break;
 
 
-      //imwrite("/tmp/test_RGB24.png", dst);
-      //imwrite("/tmp/test_RGB24_AWB.png", finalMat);
-      //break;
+        //cv::imshow("jetson", *uMat);
+        //cv::waitKey(1);
 
-      // int buffSize = (dst.total() * dst.channels());
-      // byte *buff = matToBytes(dst);
 
-      //FILE *fp=fopen("/tmp/test.img", "w+");
-      //if (fp == NULL) {
-      //  printf("Zle nedobre\n");
-      //  break;
-      //}
-      //fwrite(buff, 1, buffSize, fp);
-      //break;
+        //imwrite("/tmp/test_RGB24.png", dst);
+        //imwrite("/tmp/test_RGB24_AWB.png", finalMat);
+        //break;
 
-      measureFps();
-		}
-		
-		cam.StopAcquisition();
-		cam.Close();
-		printf("Done\n");	
+        // int buffSize = (dst.total() * dst.channels());
+        // byte *buff = matToBytes(dst);
+
+        //FILE *fp=fopen("/tmp/test.img", "w+");
+        //if (fp == NULL) {
+        //  printf("Zle nedobre\n");
+        //  break;
+        //}
+        //fwrite(buff, 1, buffSize, fp);
+        //break;
+        measureFps();
+    }
+
+    stop();
+
 }
 
