5a41d9222f54422c5197a803516215c6cf028e45
No. 42 <teamcompotes@gmail.com>
2018-05-27 16:21:18 +0200

    Fix

--- bluetooth_server.cpp
+++ bluetooth_server.cpp
@@ -4,24 +4,20 @@ using namespace std;
 
 atomic<bool> i_attack;
 
-int s, client, bytes_read;
+int s_sock, client, bytes_read;
 
 void init_bluetooth_server() {
 	
 	struct sockaddr_rc loc_addr = { 0 }, rem_addr = { 0 };
     socklen_t opt = sizeof(rem_addr);
 
-    s = socket(AF_BLUETOOTH, SOCK_STREAM, BTPROTO_RFCOMM);
+    s_sock = socket(AF_BLUETOOTH, SOCK_STREAM, BTPROTO_RFCOMM);
     loc_addr.rc_family = AF_BLUETOOTH;    
-	//loc_addr.rc_bdaddr = 0;
-    loc_addr.rc_channel = (uint8_t) 1;
-    bind(s, (struct sockaddr *)&loc_addr, sizeof(loc_addr));
-    listen(s, 1);
+	loc_addr.rc_channel = (uint8_t) 1;
+    bind(s_sock, (struct sockaddr *)&loc_addr, sizeof(loc_addr));
+    listen(s_sock, 1);
 
-    client = accept(s, (struct sockaddr *)&rem_addr, &opt);
-
-//  ba2str( &rem_addr.rc_bdaddr, buf );
-//  fprintf(stderr, "accepted connection from %s\n", buf);
+    client = accept(s_sock, (struct sockaddr *)&rem_addr, &opt);
 	
 	thread read_bluetooth(bluetooth_read_thread);
 	thread write_bluetooth(bluetooth_write_thread);
